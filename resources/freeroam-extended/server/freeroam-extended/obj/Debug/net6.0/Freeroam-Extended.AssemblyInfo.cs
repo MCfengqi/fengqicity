5cdba1e1973d1c446a80cde53d8fc5902523f9b2
MCfengqi <qxsj@vip.qq.com>
2024-11-11 12:06:14 +0800

    添加图片

--- resources/freeroam-extended/server/freeroam-extended/obj/Debug/net6.0/Freeroam-Extended.AssemblyInfo.cs
+++ resources/freeroam-extended/server/freeroam-extended/obj/Debug/net6.0/Freeroam-Extended.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Freeroam-Extended")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6015a13488c9ecccdd76eb26392eeaea90ced2f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a5c03ec39d9dade91b852d31cdee10ebecf625e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Freeroam-Extended")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Freeroam-Extended")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
